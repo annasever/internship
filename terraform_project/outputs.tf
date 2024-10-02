fa3b5c816b7cc892805cef1c54ba0054dc07cf75
root <root@ip-172-31-32-197.eu-central-1.compute.internal>
2024-10-02 07:51:59 +0000

    JF

--- terraform_project/outputs.tf
+++ terraform_project/outputs.tf
@@ -1,7 +1,7 @@
 output "frontend_ip" {
-  value = aws_instance.frontend_instance.public_ip
+  value = aws_instance.frontend.public_ip
 }
 
 output "backend_ip" {
-  value = aws_instance.backend_instance.public_ip
+  value = aws_instance.backend.public_ip
 }
