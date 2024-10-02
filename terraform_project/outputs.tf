c6c802af2a87f6ed02078a5fad6b5643117d8a24
root <root@ip-172-31-32-197.eu-central-1.compute.internal>
2024-10-02 13:26:17 +0000

    JF was changed for terraform apply

--- terraform_project/outputs.tf
+++ terraform_project/outputs.tf
@@ -6,17 +6,7 @@ output "backend_ip" {
   value = aws_instance.backend.public_ip
 }
 
-output "postgres_ip" {
-  value = aws_instance.postgres.public_ip
-  description = "Public IP of the PostgreSQL instance"
-}
-
-output "mongo_ip" {
-  value = aws_instance.mongo.public_ip
-  description = "Public IP of the MongoDB instance"
-}
-
-output "redis_ip" {
-  value = aws_instance.redis.public_ip
-  description = "Public IP of the Redis instance"
+output "databases_ip" {
+  value = aws_instance.databases.public_ip
+  description = "Public IP for DataBase instances"
 }
