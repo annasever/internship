8ca41dbafc71ef747053e34efe622147364f74ee
root <root@ip-172-31-32-197.eu-central-1.compute.internal>
2024-10-04 15:04:10 +0000

    Jenkinsfile for apply was added

--- terraform_project/security_group.tf
+++ terraform_project/security_group.tf
@@ -41,20 +41,20 @@ resource "aws_security_group" "servers_sg" {
     from_port   = 5432 # PostgreSQL
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["3.68.108.87/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 27017 # MongoDB
     to_port     = 27017
     protocol    = "tcp"
-    cidr_blocks = ["3.68.108.87/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 6379 # Redis
     to_port     = 6379
     protocol    = "tcp"
-    cidr_blocks = ["3.68.108.87/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
