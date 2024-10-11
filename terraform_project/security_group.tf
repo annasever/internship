a5e620155e4413c7cc35bd0f3a68a6c780bfc5e9
root <root@ip-172-31-32-197.eu-central-1.compute.internal>
2024-10-11 12:52:41 +0000

    git

--- terraform_project/security_group.tf
+++ terraform_project/security_group.tf
@@ -57,4 +57,33 @@ resource "aws_security_group" "servers_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # New Monitoring rules
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9093
+    to_port     = 9093
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
 }
